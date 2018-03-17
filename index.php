f7f6643a746bea34367e8dd0f237920b94641b33
Remberto Nunez <remnunez@csumb.edu>
2018-03-17 17:33:16 +0000

    background

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ if(isset($_GET['query'])) {
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <title>Products Page</title>
     </head>
-    <body>
+    <body background="bg.png">
     <div class='container'>
         <div class='text-center'>
             
